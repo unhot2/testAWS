bfe9265651ff074d619db875bc1f72d985fe1260
최윤규 <unhot2@nate.com>
2022-01-23 22:44:18 +0900

    aws test용

---
+++ testAWS/src/test/java/com/yg/aws/TestAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yg.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
